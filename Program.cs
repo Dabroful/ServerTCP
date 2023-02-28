315b9523a360a78f53d0345f04b0d76e847b2792
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-28 20:17:42 +0300

    Добавялю данные, еще нужно работать

--- Program.cs
+++ Program.cs
@@ -20,16 +20,16 @@ namespace ServerTCP
 
             while (true)                                                                                  //процесс просушивани. Должен быть бесконечным
             {
-                var listener = tcpSocket.Accept();                                                  //
-                var buffer = new byte[256];
-                var size = 0;
-                var data = new StringBuilder();
+                var listener = tcpSocket.Accept();                                                  //обработчик на приём. Он ожидает сообщение.
+                var buffer = new byte[256];                                                               //переменная - байтовый массив. Место, куда принимаем данные.
+                var size = 0;                                                                             //переменная с реальным количеством принятых байт.
+                var data = new StringBuilder();                                                           //собирает полученные данные. Стрингбилдер позволяет форматировать строки
 
-                do
+                do                                                                                        //проверем условия, что мы получили запрос
                 {
-                    size = listener.Receive(buffer);
-                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
-                } while (listener.Available > 0);
+                    size = listener.Receive(buffer);                                                      //действие получения этих байт
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));                      //добавляем данные в data. Внутри нужно раскодировать полученное сообщение. Берем данные из буфера, откуда нчинаем и количество.
+                } while (listener.Available > 0);                                                        //до тех пор пока в нашем подключении есть данные
 
                 Console.WriteLine(data);
 
